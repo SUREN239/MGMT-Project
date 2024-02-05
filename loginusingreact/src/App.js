2f346d451254a10c8ece55bb4176cb06c9407ce8
Shyam792004 <124516994+Shyam792004@users.noreply.github.com>
2024-02-05 18:38:25 +0530

    done

---
+++ loginusingreact/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import Demo from "./Components/Demo";
+import Login from "./Components/Login";
+// import { Route, Routes } from "react-router-dom";
+// import Register from "./Components/register";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login/>
+      
+    </div>
+  );
+};
+
+export default App;
